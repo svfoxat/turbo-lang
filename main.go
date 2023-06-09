e48d8b9421e03078ea0e7e778d81350949b39ffd
Nico Nößler <nico@streamstack.tv>
2023-06-09 16:31:22 +0200

    added simple repl

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Printf("Hello! This is some interpreted programming language!\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
